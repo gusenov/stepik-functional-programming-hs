8013ab7431f2f8aecbbf7bcc716b9ecedbfacc56
Аббас Гусенов <gusenov@live.ru>
2016-03-05 23:31:26 +0600

    Updated DemoFactorial/DemoFactorial.hs

--- DemoFactorial/DemoFactorial.hs
+++ DemoFactorial/DemoFactorial.hs
@@ -19,15 +19,22 @@ factorial'' n = if n < 0 then error "arg must be >= 0" else n * factorial'' (n -
 factorial''' 0 = 1
 factorial''' n | n < 0 = error "arg must be >= 0" 
                | n > 0 = n * factorial''' (n - 1)
-			   
-			   
+
+
 factorial4 :: Integer -> Integer
 factorial4 n | n == 0    = 1
              | n > 0     = n * factorial4 (n - 1)
              | otherwise = error "arg must be >= 0"
 
-			 
+
 factorial5 n | n >= 0    = helper 1 n
              | otherwise = error "arg must be >= 0"
 helper acc 0 = acc
 helper acc n = helper (acc * n) (n - 1)
+
+
+factorial6 n | n >= 0    = let
+                   helper acc 0 = acc
+                   helper acc n = helper (acc * n) (n - 1)
+               in helper 1 n
+             | otherwise = error "arg must be >= 0"
