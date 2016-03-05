a5d3d94e0455bea5b735acd69a4679c422f5d8ca
Аббас Гусенов <gusenov@live.ru>
2016-03-05 23:52:54 +0600

    Updated Roots/Roots.hs

--- Roots/Roots.hs
+++ Roots/Roots.hs
@@ -35,3 +35,10 @@ roots''' a b c =
 rootsDiff a b c = let
  (x1,x2) = roots a b c
  in x2 - x1
+
+
+roots'''' a b c = (x1, x2) where
+    x1 = (-b - d) / aTwice
+    x2 = (-b + d) / aTwice
+    d = sqrt $ b ^ 2 - 4 * a * c
+    aTwice = 2 * a
