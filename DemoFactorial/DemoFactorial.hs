bc1f45eaefe381ea7cd0eb9637f9117ea0f78f21
Аббас Гусенов <gusenov@live.ru>
2016-03-05 23:52:44 +0600

    Updated DemoFactorial/DemoFactorial.hs

--- DemoFactorial/DemoFactorial.hs
+++ DemoFactorial/DemoFactorial.hs
@@ -38,3 +38,11 @@ factorial6 n | n >= 0    = let
                    helper acc n = helper (acc * n) (n - 1)
                in helper 1 n
              | otherwise = error "arg must be >= 0"
+
+
+factorial7 :: Integer -> Integer
+factorial7 n | n >= 0    = helper 1 n
+             | otherwise = error "arg must be >= 0"
+ where
+  helper acc 0 = acc
+  helper acc n = helper (acc * n) (n - 1)
