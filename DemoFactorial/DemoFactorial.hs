39b4300f8b2c99605fb0d3e7849a74b397335d44
Аббас Гусенов <gusenov@live.ru>
2016-03-14 19:18:43 +0600

    Updated DemoFactorial/DemoFactorial.hs

--- DemoFactorial/DemoFactorial.hs
+++ DemoFactorial/DemoFactorial.hs
@@ -46,3 +46,11 @@ factorial7 n | n >= 0    = helper 1 n
  where
   helper acc 0 = acc
   helper acc n = helper (acc * n) (n - 1)
+
+
+factorial8 :: Integer -> Integer
+factorial8 n | n >= 0    = helper 1 n
+             | otherwise = error "arg must be >= 0"
+ where
+  helper acc 0 = acc
+  helper acc n = (helper $! (acc * n)) (n - 1)
