267e428b9362c5599ffdb411f6a16a961d0fc705
Аббас Гусенов <gusenov@live.ru>
2016-04-01 01:33:01 +0600

    Фукция bindLog, которая работает подобно оператору >>= для контекста Log

--- ExecLoggers/Demo.hs
+++ ExecLoggers/Demo.hs
@@ -38,3 +38,20 @@ getMsg (Log msg _) = msg
 
 add1Log = toLogger (+1) "added one"
 mult2Log = toLogger (* 2) "multiplied by 2"
+
+
+
+{-
+Реализуйте фукцию bindLog
+
+bindLog :: Log a -> (a -> Log b) -> Log b
+которая работает подобно оператору >>= для контекста Log.
+
+GHCi> Log ["nothing done yet"] 0 `bindLog` add1Log
+Log ["nothing done yet","added one"] 1
+
+GHCi> Log ["nothing done yet"] 3 `bindLog` add1Log `bindLog` mult2Log
+Log ["nothing done yet","added one","multiplied by 2"] 8
+-}
+bindLog :: Log a -> (a -> Log b) -> Log b
+bindLog (Log s a) l = (\(Log (ms) r) -> Log (s ++ ms) r) (l a)
